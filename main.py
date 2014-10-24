1bf28ba28b026b4e2b635f0c9a166515387a4b2f
Erik Aybar <erikthedeveloper@gmail.com>
2014-10-24 10:14:55 -0600

    Initial Commit - Some goodies from class

---
+++ main.py
@@ -0,0 +1,28 @@
+class LinkedList:
+    def __init__(self):
+        pass
+
+    def UnorderedUniqueContainer(self):
+        # return uuc
+        pass
+
+
+def insert(uuc):
+    pass
+
+def traverse(uuc):
+    pass
+
+def delete(uuc):
+    pass
+
+def retrieve(uuc):
+    pass
+
+
+def main():
+    uuc = LinkedList.UnorderedUniqueContainer()
+    insert(uuc)
+    traverse(uuc)
+    delete(uuc)
+    retrieve(uuc)
