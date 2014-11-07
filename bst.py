e54911077b8b4ab16c7dc100953bf09ccbb99b14
Erik Aybar <erikthedeveloper@gmail.com>
2014-11-07 10:24:47 -0700

    All tests passing or skipped (Delete, Traverse)

--- bst.py
+++ bst.py
@@ -1,32 +1,77 @@
+# https://gist.github.com/erikthedeveloper/f7d9e115537339fcceb6
+
 from uuc import UnorderedUniqueContainer
 
 
 class Bst(UnorderedUniqueContainer):
     def __init__(self):
         self._size = 0
+        self._root = None
         pass
 
-    def retrieve(self, item):
-        pass
+    def retrieve(self, dummy_item):
+        if self._root is None:
+            return False
+        return self._root.retrieve(dummy_item)
 
     def size(self):
         return self._size
 
-    def exists(self, item):
-        pass
+    def exists(self, dummy_item):
+        return bool(self.retrieve(dummy_item))
 
     def traverse(self, callback):
         pass
 
     def insert(self, item):
+        if self._root is None:
+            self._root = BstNode(item)
+        else:
+            self._root.insert(item)
         self._size += 1
-        pass
+        return True
 
     def delete(self, item):
         self._size -= 1
-        pass
+        return True
 
 
 class BstNode:
-    def __init__(self):
-        pass
+    def __init__(self, item):
+        self.left = None
+        self.right = None
+        self.item = item
+
+    def insert(self, item):
+        # Enforce Uniqueness TODO: Use self.exists(item)
+        if item == self.item:
+            return False
+        if item < self.item:
+            if self.left is None:
+                self.left = BstNode(item)
+            else:
+                self.left.insert(item)
+        elif item > self.item:
+            if self.right is None:
+                self.right = BstNode(item)
+            else:
+                self.right.insert(item)
+
+    def retrieve(self, dummy_item):
+        if dummy_item == self.item:
+            return self.item
+        if dummy_item < self.item:
+            if self.left is None:
+                return self.item
+            else:
+                return self.left.retrieve(dummy_item)
+        elif dummy_item > self.item:
+            if self.right is None:
+                return self.item
+            else:
+                return self.right.retrieve(dummy_item)
+        return False
+
+    def is_leaf(self):
+        return self.left is None and self.right is None
+
--- bst_test.py
+++ bst_test.py
@@ -1,73 +1,91 @@
+# https://gist.github.com/erikthedeveloper/f7d9e115537339fcceb6
+
 import unittest
-from bst import Bst
+
+from bst import Bst, BstNode
+
+from student import Student, StudentFactory
 
 
 class TestBst(unittest.TestCase):
     def test_container_size_is_represented(self):
         bst = Bst()
         self.assertEqual(0, bst.size())
-        bst.insert({'foo': 'bar'})
+        bst.insert(Student("Doe", "Jane", "123-123-1234", "jane@gmail.com", 23))
         self.assertEqual(1, bst.size())
-        bst.insert({'bar': 'foo'})
+        bst.insert(Student("Doe", "John", "223-123-1234", "john@gmail.com", 23))
         self.assertEqual(2, bst.size())
 
-    # def test_inserting_item_into_empty_container(self):
-    #     bst = Bst()
-    #     some_item = {'foo': 'bar'}
-    #     bst.insert(some_item)
-    #     self.assertEqual(some_item, bst.get_first().item)
-    #
-    # def test_finding_an_existing_element(self):
-    #     bst = Bst()
-    #     the_item = {'foo': 'bar'}
-    #     self.assertFalse(bst.exists(the_item))
-    #     bst.insert(the_item)
-    #     self.assertTrue(bst.exists(the_item))
-    #
-    # def test_deleting_an_item(self):
-    #     bst = Bst()
-    #
-    #     delete_me = {'foo4': 'bar'}
-    #     bst.insert({'foo2': 'bar'})
-    #     bst.insert({'foo3': 'bar'})
-    #
-    #     self.assertFalse(bst.delete(delete_me))
-    #
-    #     bst.insert(delete_me)
-    #     bst.insert({'foo5': 'bar'})
-    #
-    #     self.assertTrue(bst.delete(delete_me))
-    #
-    # def test_traverse_method(self):
-    #     bst = Bst()
-    #
-    #     characters = ["a", "b", "c"]
-    #     for c in characters:
-    #         bst.insert(c)
-    #
-    #     the_traversed_string = ""
-    #     # bst.traverse(lambda current_item: the_traversed_string)
-    #     self.assertEqual("abc", the_traversed_string)
-    #
-    # def test_exists_method(self):
-    #     bst = Bst()
-    #     find_me = {'foo4': 'bar', 'bar': 123}
-    #
-    #     self.assertFalse(bst.exists(find_me))
-    #
-    #     bst.insert(find_me)
-    #     self.assertTrue(bst.exists(find_me))
-    #
-    # def test_retrieve_method(self):
-    #     bst = Bst()
-    #     find_me = {'foo4': 'bar', 'bar': 123}
-    #
-    #     self.assertFalse(bst.retrieve(find_me))
-    #
-    #     bst.insert(find_me)
-    #     self.assertEqual(find_me, bst.retrieve(find_me))
+    def test_inserting_item_into_empty_container(self):
+        bst = Bst()
+        some_item = Student("Doe", "Jane", "123-123-1234", "jane@gmail.com", 23)
+        bst.insert(some_item)
+
+    def test_finding_an_existing_element(self):
+        bst = Bst()
+        jane = Student("Doe", "Jane", "123-123-1234", "jane@gmail.com", 23)
+        john = Student("Doe", "John", "223-123-1234", "john@gmail.com", 23)
+        jake = Student("Doe", "Jake", "323-123-1234", "john@gmail.com", 23)
+
+        dummy_john = StudentFactory.create_dummy_from_ssn("223-123-1234")
+
+        self.assertFalse(bst.exists(dummy_john))
+
+        bst.insert(jane)
+        bst.insert(john)
+        bst.insert(jake)
+
+        self.assertEqual(john, bst.retrieve(dummy_john))
+        self.assertTrue(bst.exists(dummy_john))
+
+    def test_checking_if_an_item_exists(self):
+        bst = Bst()
+        find_me = {'foo4': 'bar', 'bar': 123}
+        self.assertFalse(bst.exists(find_me))
+        bst.insert(find_me)
+        self.assertTrue(bst.exists(find_me))
+
+    def test_deleting_an_item(self):
+        self.skipTest("Not yet implemented!")
+
+        bst = Bst()
+        jane = Student("Doe", "Jane", "123-123-1234", "jane@gmail.com", 23)
+        john = Student("Doe", "John", "223-123-1234", "john@gmail.com", 23)
+        jake = Student("Doe", "Jake", "323-123-1234", "john@gmail.com", 23)
+
+        dummy_john = StudentFactory.create_dummy_from_ssn("223-123-1234")
+
+        self.assertFalse(bst.delete(dummy_john))
+
+        bst.insert(jane)
+        bst.insert(john)
+        bst.insert(jake)
+
+        self.assertTrue(bst.exists(dummy_john))
+        self.assertTrue(bst.delete(dummy_john))
+        self.assertFalse(bst.exists(dummy_john))
+
+    def test_traverse_method(self):
+        self.skipTest("Not yet implemented! Need to write useful test.")
+
+        bst = Bst()
+
+        characters = ["a", "b", "c"]
+        for c in characters:
+            bst.insert(c)
+
+        the_traversed_string = ""
+        # def append_to(new_string):
+        #     return string += new_string
+        # bst.traverse(lambda current_item: the_traversed_string)
+        self.assertEqual("abc", the_traversed_string)
 
 
+# TODO: Write tests for BstNode or remove block
+# class TestBstNode(unittest.TestCase):
+#     def test_container_size_is_represented(self):
+#         node = BstNode("Some Item")
+#         self.assertIsInstance(node, BstNode)
 
 
 if __name__ == '__main__':
