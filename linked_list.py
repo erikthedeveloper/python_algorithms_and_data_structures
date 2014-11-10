1a100f6cdbb275d4842cb32ffca96dc2e6f0bea4
Erik Aybar <erikthedeveloper@gmail.com>
2014-11-10 11:27:27 -0700

    LinkedList - Node now requires item on construct. Enforce "not exists" in insert
    main.py running and Tests passing!
    
    ... main.py so slow :'('

--- linked_list.py
+++ linked_list.py
@@ -2,18 +2,16 @@ from uuc import UnorderedUniqueContainer
 
 
 class LinkedList(UnorderedUniqueContainer):
-    """
-    :type first: Node
-    """
 
     def __init__(self):
         self._first = None
         self._node_count = 0
 
     def insert(self, item):
-        # Returns False on duplicate
-        new_node = Node()
-        new_node.item = item
+        if self.exists(item):
+            return False
+
+        new_node = Node(item)
 
         if self._first is not None:
             new_node.next = self._first
@@ -69,12 +67,12 @@ class LinkedList(UnorderedUniqueContainer):
     def size(self):
         return self._node_count
 
-    
+
 class Node:
     """
     Node implementation
     """
 
-    def __init__(self):
-        self.item = None
+    def __init__(self, item):
+        self.item = item
         self.next = None
