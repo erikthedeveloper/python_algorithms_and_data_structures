3e2129f9bcec6a83a6365c55f0259ba27fdfe871
Erik Aybar <erikthedeveloper@gmail.com>
2014-11-23 16:50:44 -0700

    Refactors bst.py - Tests passing. Consolidates excessive logic.

--- uuc_bst_and_linked_list/bst.py
+++ uuc_bst_and_linked_list/bst.py
@@ -14,8 +14,7 @@ class Bst(UnorderedUniqueContainer):
         if self.empty():
             self._root = BstNode(item)
             return True
-        else:
-            return self._root.insert(item)
+        return self._root.insert(item)
 
     def retrieve(self, dummy_item):
         if self.empty():
@@ -25,17 +24,16 @@ class Bst(UnorderedUniqueContainer):
     def delete(self, dummy_item):
         if not self.exists(dummy_item):
             return False
-
         if self._root.item == dummy_item:
             # Create a temporary "root" to encompass tree to accommodate for deleting root node as target
             temp_node = BstNode("foo")
             temp_node.left = self._root
 
-            deleted = self._root.delete(dummy_item, temp_node)
+            delete_success = self._root.delete(dummy_item, temp_node)
 
             # Set the "copied and altered" tree back to self._root
             self._root = temp_node.left
-            return deleted
+            return delete_success
 
         return self._root.delete(dummy_item, self._root)
 
@@ -62,65 +60,50 @@ class BstNode:
         self.item = item
 
     def insert(self, item):
-        if item == self.item:
-            return False
         if item < self.item:
-            if self.left is None:
-                self.left = BstNode(item)
-                return True
-            else:
+            if self.left:
                 return self.left.insert(item)
+            self.left = BstNode(item)
+            return True
         elif item > self.item:
-            if self.right is None:
-                self.right = BstNode(item)
-                return True
-            else:
+            if self.right:
                 return self.right.insert(item)
+            self.right = BstNode(item)
+            return True
+        return False
 
     def retrieve_node(self, dummy_item):
-        if dummy_item == self.item:
+        if dummy_item < self.item and self.left:
+            return self.left.retrieve_node(dummy_item)
+        elif dummy_item > self.item and self.right:
+            return self.right.retrieve_node(dummy_item)
+        elif dummy_item == self.item:
             return self
-
-        if dummy_item < self.item:
-            if self.left is None:
-                return False
-            else:
-                return self.left.retrieve_node(dummy_item)
-        elif dummy_item > self.item:
-            if self.right is None:
-                return False
-            else:
-                return self.right.retrieve_node(dummy_item)
-
         return False
 
     def retrieve(self, dummy_item):
         result_node = self.retrieve_node(dummy_item)
         if result_node:
             return result_node.item
-        else:
-            return False
+        return False
 
     def delete(self, dummy_item, parent):
-        if dummy_item < self.item and self.left:
+        if self.item == dummy_item:
+            if self.left and self.right:
+                # TODO - Refactor: Always using the "least of the greater than" will lead to an unbalanced tree.
+                self.item = self.right.smallest_child_node().item
+                self.right.delete(self.item, self)
+            elif parent.left == self:
+                parent.left = self.left if self.left else self.right
+            elif parent.right == self:
+                parent.right = self.left if self.left else self.right
+            # Leaf node case implicitly covered in above 2 cases (assigning parent.left to self.left when self.left = None)
+            return True
+        elif dummy_item < self.item and self.left:
             return self.left.delete(dummy_item, self)
         elif dummy_item > self.item and self.right:
             return self.right.delete(dummy_item, self)
-        elif dummy_item != self.item:
-            return False
-
-        # self.item == dummy_item
-        if self.left and self.right:
-            # TODO - Refactor: Always using the "least of the greater than" will lead to an unbalanced tree.
-            self.item = self.right.smallest_child_node().item
-            self.right.delete(self.item, self)
-        elif parent.left == self:
-            parent.left = self.left if self.left else self.right
-        elif parent.right == self:
-            parent.right = self.left if self.left else self.right
-        # Leaf node case implicitly covered in above 2 cases (assigning parent.left to self.left when self.left = None)
-
-        return True
+        return False
 
     def traverse(self, callable_method):
         """
